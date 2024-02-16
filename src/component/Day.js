4f7382afcc45bbd4cd949e9ad2817e6cc8b55993
uthem150 <uthem150@gmail.com>
2024-02-16 22:13:51 +0900

    useEffect, fetch로 api호출

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,15 +1,27 @@
-import dummy from "../db/data.json";
 import Word from "./Word";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 //특정 Day를 들어갔을 때, 단어들이 나오는 페이지
 export default function Day() {
-  const a = useParams(); //day/:day이므로, {day: 1}과 같은 객체 형식으로 값을 받아옴
-  const day = a.day;
+  //useParams를 이용해서, 주소창에 있는 문자를 읽어옴
+  const { day } = useParams(); //day/:day이므로, {day: 1}과 같은 객체 형식으로 값을 받아옴
+
   // 특정 day의 단어만 보이도록 filter
-  const wordList = dummy.words.filter(
-    (word) => Number(word.day) === Number(day)
-  );
+  // const wordList = dummy.words.filter(
+  //   (word) => Number(word.day) === Number(day)
+  // );
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3005/words?day=${day}`) //1일차 데이터만 가져오기 위해, ?day=1 형태로
+      .then((res) => {
+        return res.json(); //json형태로 값을 받아옴
+      })
+      .then((data) => {
+        setWords(data);
+      });
+  }, [day]); //배열에 포함된 값이 변경될 때만 useEffect가 실행 (day라는 변수에 의존성이 있다)
 
   return (
     <>
@@ -17,7 +29,7 @@ export default function Day() {
       <table>
         <tbody>
           {/* ()=>{}을 하면 {}안에 return을 넣어야 함*/}
-          {wordList.map((word) => (
+          {words.map((word) => (
             <Word word={word} key={word.id} />
           ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,20 +1,39 @@
-import dummy from "../db/data.json";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function DayList() {
-  console.log(dummy);
-  return (
-    <ul className="list_day">
-      {/* 배열을 받아서, 새로운 배열을 반환하는 map사용 */}
-      {dummy.days.map((day) => (
-        //key: 반복되는 요소에 고유한 값을 넣어야 함
+  // api에서 리스트를 가져와서 바꿔줄 것임
+  const [days, setDays] = useState([]);
 
-        <li key={day.id}>
-          {/* a태그는 클릭할 때마다 새로운 페이지를 로드하게 되는 단점 */}
-          {/* SPA (Single-Page Application)에서 페이지 전환을 부드럽게 처리하기 위해 Link태그 사용. */}
-          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-        </li>
-      ))}
-    </ul>
+  // 어떤 상태값이 변경되고, 다시 렌더링된 다음에 호출
+  useEffect(() => {
+    //api비동기 통신을 위해 fetch이용
+    fetch("http://localhost:3005/days") //api경로 적음(get요청)-> promise 반환됨
+      .then((res) => {
+        return res.json(); // response는 http응답이고, 실제 json은 아님. -> json메소드를 사용해야 함.
+        //json으로 변환되고, promise를 반환하게 됨
+      })
+      .then((data) => {
+        setDays(data);
+      });
+
+    //불필요하게 계속 실행될 수 있으니, 두번째 매개변수로 배열 전달 -> [count]count가 변경되었을 때만, 함수가 실행되도록 (의존성 배열)
+    //상태값과 무관하게 렌더링 직후 한번만 실행되는 작업은 빈 배열 전달.
+  }, []);
+  return (
+    <>
+      <ul className="list_day">
+        {/* 배열을 받아서, 새로운 배열을 반환하는 map사용 */}
+        {days.map((day) => (
+          //key: 반복되는 요소에 고유한 값을 넣어야 함
+
+          <li key={day.id}>
+            {/* a태그는 클릭할 때마다 새로운 페이지를 로드하게 되는 단점 */}
+            {/* SPA (Single-Page Application)에서 페이지 전환을 부드럽게 처리하기 위해 Link태그 사용. */}
+            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
